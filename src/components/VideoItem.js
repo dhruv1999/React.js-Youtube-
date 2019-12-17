8d887438033372f981740582871aca40670c6dd0
dhruv1999 <dhruv.anita100@gmail.com>
2019-12-17 21:10:35 +0530

    Finished

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,11 @@
 import React from "react"
 import "./VideoItem.css"
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     return (
-        <div className="video-item item">
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
             <img
+                alt={video.snippet.title}
                 className="ui image"
                 src={video.snippet.thumbnails.medium.url}
             />
